466728fb7cbe36fb5efad5facaad1dc9af63c481
ManuelNavarro7 <manuel.mnavarro@hotmail.com>
2023-02-07 19:05:07 -0300

    10 commit

--- src/Routes/routesLogin.js
+++ src/Routes/routesLogin.js
@@ -135,8 +135,9 @@ loginRouter.post(
     failureRedirect: "nne",
   }),
   async (req, res) => {
-    const { user } = req.session;
-    console.log(user)
+    // console.log(req.session)
+    const user = req.session.passport.user;
+    // console.log(user)
     try {
       const mailOptions = {
         from: "Servidor Node.js",
@@ -169,7 +170,7 @@ loginRouter.post(
 );
 
 loginRouter.get("/ruta-protegida", checkAuthentication, (req, res) => {
-  const { user } = req.session;
+  const { user } = req.session.passport.user;
   console.log(user)
   logger.info(user);
   logger.error("Parámetros incorrectos");
