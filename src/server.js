2824dd01b0c958233c1197e5016c4a2202fe264c
ManuelNavarro7 <manuel.mnavarro@hotmail.com>
2023-02-07 23:14:26 -0300

    11 commit

--- src/server.js
+++ src/server.js
@@ -70,10 +70,7 @@ const io = new Socket(httpServer,{
   }
 });
 
-app.use(cors( {
-  origin: "https://front-p-final.vercel.app",
-    methods: "GET, POST, PUT",
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
